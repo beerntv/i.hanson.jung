4d54f5759ec187391d6b1f520c299dd3a4dd914d
Hanson Jung <hansonjung@gmail.com>
2017-03-07 21:58:17 +0900

    project 새로 생성하고 다른 프로젝트에 있는 V.C 잘라다붙이고 세팅 성공. bundle ID, team 체크/ framework download/ plist - privacy - location 수

---
+++ 0307_MapView/MapView/MapView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapView
+//
+//  Created by Hanson Jung on 2017. 3. 7..
+//  Copyright © 2017년 waaoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0307_MapView/MapView/MapView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapView
+//
+//  Created by Hanson Jung on 2017. 3. 7..
+//  Copyright © 2017년 waaoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
